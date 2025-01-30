7fbdcb00f20240925d865412ef03415d935543ac
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-01-30 14:17:05 +0100

    move inventoryhaagentimpl.Module()

--- cmd/agent/subcommands/run/command.go
+++ cmd/agent/subcommands/run/command.go
@@ -102,7 +102,6 @@ import (
 	"github.com/DataDog/datadog-agent/comp/metadata/inventoryagent"
 	"github.com/DataDog/datadog-agent/comp/metadata/inventorychecks"
 	"github.com/DataDog/datadog-agent/comp/metadata/inventoryhaagent"
-	"github.com/DataDog/datadog-agent/comp/metadata/inventoryhaagent/inventoryhaagentimpl"
 	"github.com/DataDog/datadog-agent/comp/metadata/inventoryhost"
 	"github.com/DataDog/datadog-agent/comp/metadata/inventoryotel"
 	"github.com/DataDog/datadog-agent/comp/metadata/packagesigning"
@@ -476,7 +475,6 @@ func getSharedFxOption() fx.Option {
 		networkpath.Bundle(),
 		remoteagentregistryfx.Module(),
 		haagentfx.Module(),
-		inventoryhaagentimpl.Module(),
 		metricscompressorfx.Module(),
 	)
 }
