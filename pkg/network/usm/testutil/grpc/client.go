a2b056d82e36fdcfa9225ad2eaf3d6f3fde35292
Guy Arbitman <guy20495@gmail.com>
2025-03-17 10:56:05 +0200

    usm: grpc: Added test build tag flag

--- pkg/network/usm/testutil/grpc/client.go
+++ pkg/network/usm/testutil/grpc/client.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build test
+
 // Package grpc provides a gRPC client that fits the gRPC server.
 package grpc
 
