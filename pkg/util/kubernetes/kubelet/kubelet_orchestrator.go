009a70277f6fd26e210beb8d106d563631f38462
David Ortiz <david.ortiz@datadoghq.com>
2025-03-18 14:54:05 +0100

    WIP: Use JSON decoder to avoid dependency on kubernetes scheme

--- pkg/util/kubernetes/kubelet/kubelet_orchestrator.go
+++ pkg/util/kubernetes/kubelet/kubelet_orchestrator.go
@@ -9,12 +9,11 @@ package kubelet
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
 	kubeletv1alpha1 "k8s.io/kubelet/pkg/apis/stats/v1alpha1"
 )
 
@@ -44,13 +43,9 @@ func (ku *KubeUtil) GetRawLocalPodList(ctx context.Context) ([]*v1.Pod, error) {
 		return nil, fmt.Errorf("unexpected status code %d on %s%s: %s", code, ku.kubeletClient.kubeletURL, kubeletPodPath, string(data))
 	}
 
-	podListData, err := runtime.Decode(clientsetscheme.Codecs.UniversalDecoder(v1.SchemeGroupVersion), data)
-	if err != nil {
+	var podList v1.PodList
+	if err := json.Unmarshal(data, &podList); err != nil {
 		return nil, fmt.Errorf("unable to decode the pod list: %s", err)
-	}
-	podList, ok := podListData.(*v1.PodList)
-	if !ok {
-		return nil, fmt.Errorf("pod list type assertion failed on %v", podListData)
 	}
 
 	// transform []v1.Pod in []*v1.Pod
