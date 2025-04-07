8fd11bc3faffccfa4fba42c27aa23078bb827b4a
Stuart Geipel <stuart.geipel@datadoghq.com>
2025-04-07 11:04:22 +0000

    no destaddr

--- pkg/networkpath/traceroute/filter/tcp.go
+++ pkg/networkpath/traceroute/filter/tcp.go
@@ -51,7 +51,7 @@ func (c TCP4FilterConfig) GenerateTCP4Filter() ([]bpf.RawInstruction, error) {
 		// (006) ld       [30] -- load destination IP
 		bpf.LoadAbsolute{Size: 4, Off: 30 - ethHeaderSize},
 		// (007) jeq      #0x1030507       jt 8	jf 16 -- if dstAddr matches, goto 8, else 16
-		bpf.JumpIf{Cond: bpf.JumpEqual, Val: dstAddr, SkipTrue: 0, SkipFalse: 8},
+		bpf.JumpIf{Cond: bpf.JumpEqual, Val: dstAddr, SkipTrue: 0, SkipFalse: 0}, // tmp disable
 		// (008) ldh      [20] -- load Fragment Offset
 		bpf.LoadAbsolute{Size: 2, Off: 20 - ethHeaderSize},
 		// (009) jset     #0x1fff          jt 16	jf 10 -- if fragmented, goto 16, else 10
