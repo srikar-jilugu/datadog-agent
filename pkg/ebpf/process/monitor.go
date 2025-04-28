31091c8a20933ff17cb521543277e22935b88cb6
Bryce Kahle <bryce.kahle@datadoghq.com>
2025-04-28 11:17:08 -0700

    add missing build tags

--- pkg/ebpf/process/monitor.go
+++ pkg/ebpf/process/monitor.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
+//go:build linux
+
 // Package process provides eBPF-based utilities for monitoring process events
 package process
 
