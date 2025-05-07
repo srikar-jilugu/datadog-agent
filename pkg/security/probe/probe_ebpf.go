488642cd53b9b6cb08c1ab008e9c33f454f43247
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2025-05-07 17:03:32 +0200

    [CWS] add a ringbuf usage threshold

--- pkg/security/probe/probe_ebpf.go
+++ pkg/security/probe/probe_ebpf.go
@@ -2249,6 +2249,14 @@ func (p *EBPFProbe) initManagerOptionsConstants() {
 			Name:  "raw_packet_limiter_rate",
 			Value: uint64(p.config.Probe.NetworkRawPacketLimiterRate),
 		},
+		manager.ConstantEditor{
+			Name:  "ring_buffer_size",
+			Value: uint64(p.managerOptions.MapSpecEditors["events"].MaxEntries),
+		},
+		manager.ConstantEditor{
+			Name:  "ring_buffer_threshold",
+			Value: uint64(p.config.Probe.EventStreamBufferThreshold),
+		},
 	)
 
 	if p.kernelVersion.HavePIDLinkStruct() {
@@ -2390,9 +2398,9 @@ func (p *EBPFProbe) initManagerOptions() error {
 
 	p.managerOptions = ebpf.NewDefaultOptions(kretprobeMaxActive)
 	p.initManagerOptionsActivatedProbes()
+	p.initManagerOptionsMapSpecEditors()
 	p.initManagerOptionsConstants()
 	p.initManagerOptionsTailCalls()
-	p.initManagerOptionsMapSpecEditors()
 	return p.initManagerOptionsExcludedFunctions()
 }
 
